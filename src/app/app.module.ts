0177e68c189521aa0af5d14bd503126c221de113
Dalton Dayton <daltondayton1@gmail.com>
2019-09-11 09:12:20 -0600

    Add page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -38,6 +39,7 @@ const appRoutes: Routes = [
         UserComponent,
         EditServerComponent,
         ServerComponent,
+        PageNotFoundComponent,
     ],
     imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
     providers: [ServersService],
